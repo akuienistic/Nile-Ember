3dbdb5b3bca56a12cc839cb591578974b28729ca
Simon Akuien Atem <akuienatem80@gmail.com>
2026-03-05 23:06:02 +0300

    NE v1.0.1 Made overall modifications to the site

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -145,17 +145,6 @@ const HeroSection = () => {
           </motion.button>
         </motion.div>
       </motion.div>
-
-      {/* Scroll indicator */}
-      <motion.div
-        animate={{ y: [0, 10, 0] }}
-        transition={{ repeat: Infinity, duration: 2 }}
-        className="absolute bottom-8 left-1/2 -translate-x-1/2 z-10"
-      >
-        <div className="w-6 h-10 border-2 border-gold/50 rounded-full flex items-start justify-center p-1">
-          <div className="w-1.5 h-3 bg-gold rounded-full" />
-        </div>
-      </motion.div>
     </section>
   );
 };
